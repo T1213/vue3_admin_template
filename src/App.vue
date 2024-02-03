defbe875bcddd095d7611a55aa721a14b04f6459
T1213 <1711917202@qq.com>
2024-02-03 16:24:10 +0800

    提交未格式化代码

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>我是APP</div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/main.ts
@@ -0,0 +1,9 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+createApp(App).mount('#app')
+
+
+function fn(){
+    console.log("为格式代码");
+}
